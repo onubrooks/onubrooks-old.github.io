23bcf3b5155c181983b2a4b6d262670657d44dd9
onu abah <abahonuh@gmail.com>
2018-06-27 15:17:48 +0100

    file structure reshuffle

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const staticOptions = {
 //app.set("view engine", "html");
 // disable layout
 
-app.use('/js', express.static('currency-converter/dist/js', staticOptions));
-app.use('/css', express.static('currency-converter/dist/css', staticOptions));
-app.use('/img', express.static('currency-converter/dist/img', staticOptions));
+app.use('/dist/js', express.static('currency-converter/dist/js', staticOptions));
+app.use('/dist/css', express.static('currency-converter/dist/css', staticOptions));
+app.use('/dist/img', express.static('currency-converter/dist/img', staticOptions));
 app.get('/sw.js', (req, res) => res.sendFile('sw.js', staticOptions));
 
 app.get("/", (req, res) => res.sendFile('index.html', staticOptions));
